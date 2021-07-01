88903d8a7eecdd8a42895b93f3910c2f79f5b5f8
lightninglife <zhaofeng8711@gmail.com>
2021-07-01 14:36:25 -0400

    Update vpc.tf

--- terraform-vpc-3-tiers-extended/vpc.tf
+++ terraform-vpc-3-tiers-extended/vpc.tf
@@ -386,6 +386,19 @@ resource "aws_security_group" "alb_sg" {
 ### Create an IAM role
 resource "aws_iam_role" "eks_role" {
   name = "eks_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
   tags = {
     tag-key = "eks_role"
   }
